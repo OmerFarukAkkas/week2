1c3244bdaa5f978f6e426ab5e3f25cab2da21fd6
OmerFarukAkkas <omerfarukakkas9@gmail.com>
2024-10-28 01:53:26 +0300

    dictionary olusturulmasi ve yazdirilmasi

---
+++ consoleApp.py
@@ -0,0 +1,13 @@
+import random
+
+student1 = {'isim':'Omer Faruk' , 'soyisim':'Akkas' , 'numara':1140110000+random.randint(0,1000) , 'sinif':random.randint(1,4)}
+student2 = {'isim':'Adil' , 'soyisim':'Batur' , 'numara':1140110000+random.randint(0,1000) , 'sinif':random.randint(1,4)}
+student3 = {'isim':'Batuhan' , 'soyisim':'Ak' , 'numara':1140110000+random.randint(0,1000) , 'sinif':random.randint(1,4)}
+student4 = {'isim':'Kursat' , 'soyisim':'Akin' , 'numara':1140110000+random.randint(0,1000) , 'sinif':random.randint(1,4)}
+student5 = {'isim':'Mustafa' , 'soyisim':'Sen' , 'numara':1140110000+random.randint(0,1000) , 'sinif':random.randint(1,4)}
+
+print(student1)
+print(student2)
+print(student3)
+print(student4)
+print(student5)
